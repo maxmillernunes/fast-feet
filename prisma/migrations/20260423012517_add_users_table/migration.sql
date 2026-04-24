b3ea402b915807e3da6feb15c8479ed391720426
Maxmiller Nunes <maxmillernunes11@gmail.com>
2026-04-23 21:45:09 -0300

    refactor: :recycle: refactor prisma tables

---
+++ prisma/migrations/20260423012517_add_users_table/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('DRIVER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'DRIVER',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_document_key" ON "users"("document");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
