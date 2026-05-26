6d65b16a8558d88b3fb7cf88cbb610344bcdd256
Maxmiller Nunes <maxmillernunes11@gmail.com>
2026-05-26 00:54:20 -0300

    feat: :sparkles: add the controller to read notification

---
+++ src/infra/http/controllers/read-notification.controller.ts
@@ -0,0 +1,33 @@
+import { ReadNotificationUseCase } from '@/domain/notification/application/use-cases/read-notification'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import type { UserPayload } from '@/infra/auth/jwt.strategy'
+import {
+  BadRequestException,
+  Controller,
+  HttpCode,
+  Param,
+  Patch,
+} from '@nestjs/common'
+
+@Controller('notifications/:notificationId/read')
+export class ReadNotificationController {
+  constructor(private readNotification: ReadNotificationUseCase) {}
+
+  @Patch()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('notificationId') notificationId: string,
+  ) {
+    const result = await this.readNotification.execute({
+      notificationId,
+      recipientId: user.sub,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      throw new BadRequestException(error.message)
+    }
+  }
+}
